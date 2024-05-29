3e5dd2e016af357ac88343675598796726a8b603
koppunuruneeraj <168514823+koppunuruneeraj@users.noreply.github.com>
2024-05-29 23:01:14 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "filename" {
+default ="root/pets.txt"
+type = string
+}
+variable "content" {
+default ="we love pets!"
+type = string
+}
+variable "prefix" {
+default ="MR"
+type = string
+}
+variable "length" {
+default ="1"
+type = number
+}
